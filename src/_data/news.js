b8551e09b457dda6fc072b91e9c230e888f750d6
Thabiso <thabisoma@bbd.co.za>
2021-05-10 17:51:55 +0200

    update api_key

--- src/_data/news.js
+++ src/_data/news.js
@@ -4,12 +4,10 @@ require("dotenv").config();
 module.exports = async function getUser() {
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/everything?q=tesla&from=2021-04-10&sortBy=publishedAt&apiKey=${process.env.API_KEY}&pageSize=6`
+      `https://newsapi.org/v2/everything?q=tesla&from=2021-04-10&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
     );
     return response.data;
   } catch (error) {
     console.error(error);
   }
 };
-
-// getUser();
